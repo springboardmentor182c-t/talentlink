d59ad1fd46db0432c247565864f871243d946a24
Siddharth Kumar <siddharthkumaryo@gmail.com>
2025-12-11 23:02:18 +0530

    Project Proposal Submission UI

--- server/apps/messaging/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import ConversationListView, MessageListCreateView
-
-urlpatterns = [
-    path('conversations/', ConversationListView.as_view()),
-    path('conversations/<int:conversation_id>/messages/', MessageListCreateView.as_view()),
-]
